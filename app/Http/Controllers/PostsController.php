cb66bc66e3a0151b93371efc662bbd967ffae456
Hider Wang <724598894@qq.com>
2018-01-24 23:48:33 +0800

    前台界面

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PostController extends Controller
+class PostsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return view('post/index');
     }
 
     /**
@@ -23,7 +23,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('post/create');
     }
 
     /**
@@ -34,7 +34,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return view('post/store');
     }
 
     /**
@@ -45,7 +45,7 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('post/show');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,14 @@
 */
 
 //文章列表页
-Route::get('/posts','PostsController@index');
+Route::get('/posts', 'PostsController@index');
 //文章详情页
-Route::get('/posts/{post}','PostsController@show');
+Route::get('/posts/{post}', 'PostsController@show');
 //创建文章
-Route::get('/posts/create','PostsController@create');
-Route::post('/posts','PostsController@store');
+Route::get('/posts/create', 'PostsController@create');
+Route::post('/posts', 'PostsController@store');
 //编辑文章
-Route::get('/posts/{post}/edit','PostsController@edit');
-Route::put('/posts/{post}','PostsController@update');
+Route::get('/posts/{post}/edit', 'PostsController@edit');
+Route::put('/posts/{post}', 'PostsController@update');
 //删除文章
-Route::get('/posts/delete','PostsController@delete');
+Route::get('/posts/delete', 'PostsController@delete');
